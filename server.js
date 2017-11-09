23f7f58863a8b9b8901ae387af3c6c7896ba7491
The Whale <glittle@gmail.com>
2017-11-08 18:12:23 -0800

    bloop

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var diffsync = require('./diffsync.js')
 console.log('diffsync version ' + diffsync.version)
 
 var bus = require('statebus')()
-bus.file_store()
+bus.sqlite_store()
 
 var channels = {}
 for (var key in bus.cache) {
